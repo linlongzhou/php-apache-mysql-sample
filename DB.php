ad08fc6e70689ed7ccc3d07939a4532aa90ec918
linlongzhou <lin_longzhou@qq.com>
2016-11-08 17:28:53 +0800

    update

--- DB.php
+++ DB.php
@@ -8,15 +8,10 @@ class DB
 
     function __construct()
     {
-        $serverName =   env("MYSQL_PORT_3306_TCP_ADDR", "localhost");
-        $databaseName = env("MYSQL_INSTANCE_NAME", "homestead");
-        $username =     env("MYSQL_USERNAME", "homestead");
-        $password =     env("MYSQL_PASSWORD", "secret");
         
-        echo "" . $serverName;        
 
         try {
-            $this->pdo = new PDO("mysql:host=$serverName;dbname=$databaseName", $username, $password);
+            $this->pdo = new PDO("mysql:host=db1.daocloudinternal.io:60115;dbname=temp_db", "root", "GuMvZmvy");
 
             $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
